1689ed7c49edac095210682229c05655cc8d84f1
JINJIALEI <admin@dullme.com>
2019-03-26 20:22:47 +0800

    完成

--- app/Foundation/SingleLoginAuthenticatesUsers.php
+++ app/Foundation/SingleLoginAuthenticatesUsers.php
@@ -22,8 +22,11 @@ trait SingleLoginAuthenticatesUsers
         $this->clearLoginAttempts($request);
         #这里来做登录后的操作
         $this->singleLogin($request);
-        return $this->authenticated($request, $this->guard()->user())
-            ?: redirect()->intended($this->redirectPath());
+
+        return redirect()->to(url($this->redirectPath()));
+//
+//        return $this->authenticated($request, $this->guard()->user())
+//            ?: redirect()->intended($this->redirectPath());
     }
 
     /**
