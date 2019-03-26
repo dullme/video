f342081720c2977dcf1e08c95b2fdc0171d3f887
JINJIALEI <admin@dullme.com>
2019-03-26 22:59:07 +0800

    完成

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -3,32 +3,32 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Recharge;
+use App\Suggest;
+use App\User;
+use App\Video;
 use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
+use Encore\Admin\Widgets\InfoBox;
 
 class HomeController extends Controller
 {
     public function index(Content $content)
     {
-        return $content
-            ->header('Dashboard')
-            ->description('Description...')
-            ->row(Dashboard::title())
-            ->row(function (Row $row) {
+        $content->row(function ($row) {
+            $users = User::all()->count();
+            $videos = Video::all()->count();
+            $recharge = Recharge::where('status', 'PAID')->sum('amount');
+            $suggests = Suggest::all()->count();
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
-                });
+            $row->column(3, new InfoBox('会员总数', 'users', 'aqua', '/admin/user', $users));
+            $row->column(3, new InfoBox('视频总数', 'video-camera', 'green', '/admin/video', $videos));
+            $row->column(3, new InfoBox('充值总金额', 'cc-visa', 'yellow', '/admin/recharge', $recharge/100));
+            $row->column(3, new InfoBox('意见建议', 'file', 'red', '/admin/suggest', $suggests));
+        });
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
-                });
-            });
+        return $content;
     }
 }
