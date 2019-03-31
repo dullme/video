7364964040558913d3938a30fc226003d487e12b
JINJIALEI <admin@dullme.com>
2019-03-31 22:21:11 +0800

    完成

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use App\Recharge;
 use App\Suggest;
 use App\User;
 use App\Video;
+use Carbon\Carbon;
 use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
@@ -18,12 +19,12 @@ class HomeController extends Controller
     public function index(Content $content)
     {
         $content->row(function ($row) {
-            $users = User::all()->count();
+            $users = User::where('expire_at', '>', Carbon::now())->count();
             $videos = Video::all()->count();
             $recharge = Recharge::where('status', 'PAID')->sum('amount');
             $suggests = Suggest::all()->count();
 
-            $row->column(3, new InfoBox('会员总数', 'users', 'aqua', '/admin/user', $users));
+            $row->column(3, new InfoBox('未过期会员', 'users', 'aqua', '/admin/user', $users));
             $row->column(3, new InfoBox('视频总数', 'video-camera', 'green', '/admin/video', $videos));
             $row->column(3, new InfoBox('充值总金额', 'cc-visa', 'yellow', '/admin/recharge', $recharge/100));
             $row->column(3, new InfoBox('意见建议', 'file', 'red', '/admin/suggest', $suggests));
