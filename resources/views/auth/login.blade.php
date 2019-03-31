be302655adffa6eb9ecdf0aaf03876adda922038
JINJIALEI <admin@dullme.com>
2019-03-31 17:01:44 +0800

    完成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,65 +21,68 @@
 
 <div class="py-4 container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 news">
             <div class="card">
-                <div class="card-header" style="text-align: center;font-size: 2rem">{{ config('login_text') }}</div>
+                <div class="card-header" style="text-align: center;font-size: 2rem;color: white">{{ config('login_text') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">用户名</label>
-
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus oncopy="return false" onpaste="return false" oncut="return false">
-
-                                @if ($errors->has('username'))
-                                    <span class="invalid-feedback" role="alert">
+                            <div class="col-md-12">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="inputGroupPrepend"><i class="fa fa-user"></i></span>
+                                    </div>
+                                    <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus oncopy="return false" onpaste="return false" oncut="return false" placeholder="用户名">
+                                    @if ($errors->has('username'))
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('username') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">密码</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required oncopy="return false" onpaste="return false" oncut="return false">
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
+                            <div class="col-md-12">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="inputGroupPrepend"><i class="fa fa-unlock-alt"></i></span>
+                                    </div>
+                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required oncopy="return false" onpaste="return false" oncut="return false" placeholder="密码">
+                                    @if ($errors->has('password'))
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="captcha" class="col-md-4 col-form-label text-md-right">验证码</label>
-
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="input-group">
-                                    <img src="{{captcha_src()}}" onclick="this.src='{{captcha_src()}}'+Math.random()">
-                                    <input type="text" class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}" name="captcha">
+                                    <div class="input-group-prepend">
+                                        <img src="{{captcha_src()}}" onclick="this.src='{{captcha_src()}}'+Math.random()">
+                                    </div>
+                                    <input type="text" class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}" name="captcha" placeholder="验证码">
                                     @if ($errors->has('captcha'))
                                         <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('captcha') }}</strong>
                                     </span>
                                     @endif
                                 </div>
-
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-12 text-right">
+                                <button type="submit" class="btn btn-primary btn-block">
                                     登陆
                                 </button>
-                                <a style="margin-left: 10px;" href="{{ url('/register') }}">去注册</a>
+                                <a style="display: block;margin-top: 1rem;color: white;" href="{{ url('/register') }}">去注册</a>
                             </div>
                         </div>
                     </form>
